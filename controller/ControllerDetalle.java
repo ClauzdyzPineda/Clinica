2247722b940a0da9c99778c26a4eb3811c6de24d
ClauzdyzPineda <61422522+ClauzdyzPineda@users.noreply.github.com>
2020-07-02 23:54:42 -0600

    Add files via upload

--- controller/ControllerDetalle.java
+++ controller/ControllerDetalle.java
@@ -1,8 +1,9 @@
-
 package controller;
 
 import ejb.DetalleFacadeLocal;
+import entity.Cita;
 import entity.Detalle;
+import entity.Estado;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
@@ -16,11 +17,13 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 public class ControllerDetalle implements Serializable {
 
-   @EJB
-   private DetalleFacadeLocal detalleEJB;
-   private Detalle detalle;
-   private List<Detalle> lista;
-   private String mensaje;
+    @EJB
+    private DetalleFacadeLocal detalleEJB;
+    private Detalle detalle;
+    private Cita cita;
+    private Estado estado;
+    private List<Detalle> lista;
+    private String mensaje;
 
     public Detalle getDetalle() {
         return detalle;
@@ -30,6 +33,23 @@ public class ControllerDetalle implements Serializable {
         this.detalle = detalle;
     }
 
+    public Cita getCita() {
+        return cita;
+    }
+
+    public void setCita(Cita cita) {
+        this.cita = cita;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    
+
     public List<Detalle> getLista() {
         lista = detalleEJB.findAll();
         return lista;
@@ -38,18 +58,24 @@ public class ControllerDetalle implements Serializable {
     public void setLista(List<Detalle> lista) {
         this.lista = lista;
     }
-   
-     @PostConstruct
+
+    @PostConstruct
     public void init() {
         this.detalle = new Detalle();
         lista = detalleEJB.findAll();
+        this.cita = new Cita();
+        this.estado = new Estado();
     }
 
     public void create() {
         try {
+            detalle.setId_cita(cita);
+            detalle.setId_estado(estado);
             detalleEJB.create(detalle);
             mensaje = "Datos guardados correctamente";
             detalle = new Detalle();
+            cita = new Cita();
+            estado = new Estado();
         } catch (Exception e) {
             mensaje = "Error al guardar" + e.getMessage();
         }
@@ -76,8 +102,13 @@ public class ControllerDetalle implements Serializable {
 
     public void update() {
         try {
+            detalle.setId_cita(cita);
+            detalle.setId_estado(estado);
             detalleEJB.edit(detalle);
             mensaje = "Datos Actualizados";
+            detalle = new Detalle();
+            estado = new Estado();
+            cita = new Cita();
         } catch (Exception e) {
         }
 
@@ -87,8 +118,11 @@ public class ControllerDetalle implements Serializable {
 
     public void leerId(Detalle dt) {
         try {
-            detalle = detalleEJB.find(dt.getId_detalle());
-            detalleEJB.find(dt.getId_detalle());
+           cita.setId_cita(dt.getId_cita().getId_cita());
+           estado.setId_estado(dt.getId_estado().getId_estado());
+           detalle.setId_cita(cita);
+           detalle.setId_estado(estado);
+           detalle = dt;
         } catch (Exception e) {
         }
     }
